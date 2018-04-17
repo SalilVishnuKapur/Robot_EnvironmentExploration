e447c6f08ba4c6251c026b8624cad1a48892e48d
SalilVishnuKapur <salilvishnukapur@gmail.com>
2018-04-17 15:35:25 -0300

    Modified this module to be a class

--- util.py
+++ util.py
@@ -1,14 +1,19 @@
+import math
 
-def custom_meshgrid(list1,list2):
-   list1_length = len(list1)
-   list2_length = len(list2)
-   XX_c = []
-   for i in range(list2_length):
-       XX_c.append(list1)
-   YY_c = []
-   for x in list2:
-       list1 = []
-       for i in range(list1_length):
-           list1.append(x)
-       YY_c.append(list1)
-   return XX_c,YY_c
+class Util:
+    def deg2rad(x):
+        return(x * math.pi / 180)
+
+    def custom_meshgrid(list1,list2):
+       list1_length = len(list1)
+       list2_length = len(list2)
+       XX_c = []
+       for i in range(list2_length):
+           XX_c.append(list1)
+       YY_c = []
+       for x in list2:
+           list1 = []
+           for i in range(list1_length):
+               list1.append(x)
+           YY_c.append(list1)
+       return XX_c,YY_c
