15468153fde8f99ec751eb2325377639ecca2eee
parmeet <parmeet.singh262@gmail.com>
2018-04-17 14:56:13 -0300

    utilities

---
+++ util.py
@@ -0,0 +1,14 @@
+
+def custom_meshgrid(list1,list2):
+   list1_length = len(list1)
+   list2_length = len(list2)
+   XX_c = []
+   for i in range(list2_length):
+       XX_c.append(list1)
+   YY_c = []
+   for x in list2:
+       list1 = []
+       for i in range(list1_length):
+           list1.append(x)
+       YY_c.append(list1)
+   return XX_c,YY_c
